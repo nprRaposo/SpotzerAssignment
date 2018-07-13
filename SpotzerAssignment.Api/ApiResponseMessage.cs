ef48fc5c7d3bcca429508b72b106579d3ed5cdb1
nprraposo <Nicolas.X.Raposo.-ND@disney.com>
2018-07-12 21:33:02 -0300

    Adding error handling

---
+++ SpotzerAssignment.Api/ApiResponseMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpotzerAssignment.Api
+{
+    public class ApiResponseMessage
+    {
+        public bool Success { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
